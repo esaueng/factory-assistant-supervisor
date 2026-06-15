aec5fe3faf3ba1f6ff079ec487c23cd2c8e77705
Peter <171875562+petergustafson@users.noreply.github.com>
2026-06-14 20:38:44 -0400

    update

--- supervisor/const.py
+++ supervisor/const.py
@@ -22,7 +22,9 @@ OBSERVER_DOCKER_NAME: str = f"{DOCKER_PREFIX}_observer"
 SUPERVISOR_DOCKER_NAME: str = f"{DOCKER_PREFIX}_supervisor"
 
 URL_HASSIO_ADDONS = "https://github.com/home-assistant/addons"
-URL_HASSIO_APPARMOR = "https://esaueng.github.io/FactoryAssistantOS/apparmor_{channel}.txt"
+URL_HASSIO_APPARMOR = (
+    "https://esaueng.github.io/FactoryAssistantOS/apparmor_{channel}.txt"
+)
 URL_HASSIO_VERSION = "https://esaueng.github.io/FactoryAssistantOS/{channel}.json"
 
 SUPERVISOR_DATA = Path("/data")
